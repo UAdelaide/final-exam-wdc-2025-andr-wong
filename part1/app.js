9d169c4dab882ee4c7e7c709fdd24b4945e5daa4
“Andrew <andrew.wong@student.adelaide.edu.au>
2025-06-20 05:38:21 +0000

    [automatic commit] 2025-06-20 05:38:21 UTC

--- part1/app.js
+++ part1/app.js
@@ -90,7 +90,8 @@ app.get('/api/walkrequests/open', async (req,res) => {
             JOIN Users u ON d.owner_id = u.user_id
             WHERE wr.status = 'open'
             ORDER BY wr.requested_time
-            `)
+            `);
+        res.json(walkRequests);
     }
 })
 
