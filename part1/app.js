05444cfeddfbe51a762b87efe797fbeff9f521c1
“Andrew <andrew.wong@student.adelaide.edu.au>
2025-06-20 04:07:37 +0000

    [automatic commit] 2025-06-20 04:07:37 UTC

--- part1/app.js
+++ part1/app.js
@@ -63,7 +63,8 @@ app.get('/api/dogs', async (req, res) =>{
             SELECT
                 d.name AS dog_name,
                 d.size,
-                
+                u.username AS owner_
+
             ')
     }
 })
