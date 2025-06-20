9c4a0935e0c636e792147113152fd3052ae6e741
“Andrew <andrew.wong@student.adelaide.edu.au>
2025-06-20 02:13:57 +0000

    [automatic commit] 2025-06-20 02:13:56 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -62,3 +62,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('johnny72', 'johnny@example.com', 'hashed222', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
