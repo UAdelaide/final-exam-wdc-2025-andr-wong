a18f79a49fd5e7e08b3bb5104e36fea01ca19759
“Andrew <andrew.wong@student.adelaide.edu.au>
2025-06-20 04:22:54 +0000

    [automatic commit] 2025-06-20 04:22:54 UTC

--- part1/app.js
+++ part1/app.js
@@ -77,7 +77,7 @@ app.get('/api/dogs', async (req, res) => {
 // API ROUTE: WALK REQUESTS
 app.get('/api/walkers/summary', async (req, res) => {
     try {
-        
+        const [walkerSummary] = await db.execeute
     }
 })
 
