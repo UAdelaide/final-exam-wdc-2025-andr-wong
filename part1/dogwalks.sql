91f7ac69eac1ce8ec6d39a398dad9106019c4fa4
“Andrew <andrew.wong@student.adelaide.edu.au>
2025-06-20 02:12:55 +0000

    [automatic commit] 2025-06-20 02:12:55 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -57,3 +57,7 @@ CREATE TABLE WalkRatings (
 INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+('carol123', 'carol@example.com', 'hashed789', 'owner'),
+('andywalker', 'andy@example.com', 'hashed111', 'walker'),
+('johnny72', 'johnny@example.com', 'hashed222', 'owner');
+
